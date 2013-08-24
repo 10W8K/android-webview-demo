9fa07024606988f4712cbc17dc5b0e9fc10392eb
想当当 <donghua.yan@alipay.com>
2013-08-24 20:19:38 +0800

    update

---
+++ src/com/example/WebView/MainView.java
@@ -0,0 +1,30 @@
+package com.example.WebView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 13-8-24
+ * Time: 下午8:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainView extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.index);
+
+        /* 新建一个Intent对象 */
+        Intent intent = new Intent();
+        intent.putExtra("name","LeiPei");
+        /* 指定intent要启动的类 */
+        intent.setClass(MainView.this, HelloWebView.class);
+        /* 启动一个新的Activity */
+        MainView.this.startActivity(intent);
+        /* 关闭当前的Activity */
+        MainView.this.finish();
+    }
+}
