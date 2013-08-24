e50525d8b4057d498607400d3c7a6c90bdcf8455
想当当 <donghua.yan@alipay.com>
2013-08-24 21:43:52 +0800

    update

--- src/com/example/WebView/MainView.java
+++ src/com/example/WebView/MainView.java
@@ -3,6 +3,8 @@ package com.example.WebView;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,20 +14,29 @@ import android.os.Bundle;
  * To change this template use File | Settings | File Templates.
  */
 public class MainView extends Activity {
+    Button button;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main);
 
-        /* 新建一个Intent对象 */
-        Intent intent = new Intent();
-        intent.putExtra("name","LeiPei");
-        /* 指定intent要启动的类 */
-        intent.setClass(MainView.this, HelloWebView.class);
-        /* 启动一个新的Activity */
-        startActivity(intent);
-        //HelloWebView.this.startActivity(intent);
-        /* 关闭当前的Activity */
-        MainView.this.finish();
+
+        button  = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+
+                /* 新建一个Intent对象 */
+                Intent intent = new Intent();
+                intent.putExtra("name","LeiPei");
+                /* 指定intent要启动的类 */
+                intent.setClass(MainView.this, HelloWebView.class);
+                /* 启动一个新的Activity */
+                startActivity(intent);
+                //HelloWebView.this.startActivity(intent);
+                /* 关闭当前的Activity */
+                MainView.this.finish();
+            }
+        });
+
     }
 }
